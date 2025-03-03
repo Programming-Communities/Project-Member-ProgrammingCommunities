63831e3ca58c560f4ec9d09f18f72bba74bfc94c
Aamir Shahzadah <aamirshahzadah@gmail.com>
2025-03-03 06:54:51 +0500

    Hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,29 +1,20 @@
-import { BackgroundBeams } from '@/components/ui/background-beams'
+import { BackgroundLines } from "@/components/ui/background-lines";
+
 import React from 'react'
 
 function page() {
   return (
-    <div className="h-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
-    <div className="max-w-2xl mx-auto p-4">
-      <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-      Programming Communities
-      </h1>
-      <p></p>
-      <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
-        Welcome to MailJet, the best transactional email service on the web.
-        We provide reliable, scalable, and customizable email solutions for
-        your business. Whether you&apos;re sending order confirmations,
-        password reset emails, or promotional campaigns, MailJet has got you
-        covered.
-      </p>
-      <input
-        type="text"
-        placeholder="hi@amir.com.pk"
-        className="rounded-lg border border-neutral-800 focus:ring-2 focus:ring-teal-500  w-full relative z-10 mt-4  bg-neutral-950 placeholder:text-neutral-700"
-      />
-    </div>
-    <BackgroundBeams />
-  </div>
+   
+    <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
+    <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+    Programming  <br /> Communities
+    </h2>
+    <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
+    is a free source of learning different skills..
+    We are going to start different different sessions on different topic.
+    </p>
+  </BackgroundLines>
+  
   )
 }
 
